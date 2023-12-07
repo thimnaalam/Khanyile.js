f7a22ac344cec6127acf7990cba463dece5fec27
thimnaalam <alamthimna@gmail.com>
2023-12-07 15:05:04 +0200

    CheckoutAdmin

--- script/admin.js
+++ script/admin.js
@@ -1,3 +1,79 @@
 // footer
 let currentDate = new Date().getUTCFullYear();
 document.querySelector('currentDate').textContent = currentDate;
+//table
+// document.addEventListener('DOMContentLoaded', function () {
+//     const productTableBody = document.querySelector('#productTable tbody');
+//     const addProductForm = document.getElementById('addProductForm');
+
+//     let products = JSON.parse(localStorage.getItem('products')) || [];
+
+//     // Initial rendering of products
+//     renderProducts();
+
+//     // Event listeners
+//     addProductForm.addEventListener('submit', addNewProduct);
+
+//     // Functions
+//     function renderProducts() {
+//         productTableBody.innerHTML = '';
+//         products.forEach(product => {
+//             const row = document.createElement('tr');
+//             row.innerHTML = `
+//                 <td>${product.id}</td>
+//                 <td>${product.name}</td>
+//                 <td>${product.amount}</td>
+//                 <td><img src="${product.image}" alt="${product.name}" style="max-height: 50px;"></td>
+//                 <td>
+//                     <button onclick="editProduct(${product.id})">Edit</button>
+//                     <button onclick="deleteProduct(${product.id})">Delete</button>
+//                 </td>
+//             `;
+//             productTableBody.appendChild(row);
+//         });
+//     }
+
+//     function addNewProduct(event) {
+//         event.preventDefault();
+
+//         const newName = document.getElementById('newProductName').value;
+//         const newAmount = parseFloat(document.getElementById('newProductAmount').value);
+//         const newImage = document.getElementById('newProductImage').value;
+
+//         if (newName && !isNaN(newAmount) && newAmount >= 0 && newImage) {
+//             const newProduct = {
+//                 id: products.length + 1,
+//                 name: newName,
+//                 amount: newAmount,
+//                 image: newImage,
+//             };
+
+//             products.push(newProduct);
+//             saveProductsToLocalStorage();
+//             renderProducts();
+
+//             // Clear the form fields
+//             addProductForm.reset();
+//         } else {
+//             alert('Please fill in all the fields with valid data.');
+//         }
+//     }
+
+//     function editProduct(productId) {
+//         // Implement edit functionality as needed
+//         console.log('Edit product with ID:', productId);
+//     }
+
+//     function deleteProduct(productId) {
+//         const confirmDelete = confirm('Are you sure you want to delete this product?');
+//         if (confirmDelete) {
+//             products = products.filter(product => product.id !== productId);
+//             saveProductsToLocalStorage();
+//             renderProducts();
+//         }
+//     }
+
+//     function saveProductsToLocalStorage() {
+//         localStorage.setItem('products', JSON.stringify(products));
+//     }
+// });
--- script/products.js
+++ script/products.js
@@ -1,53 +1,72 @@
 
 
+let cart = JSON.parse(localStorage.getItem('checkout')) || []
+//  product data sample  
+let products = JSON.parse(localStorage.getItem('products')) ||
+ [
+    // product data
+    {
+        id: 1,
+        name: 'Super-seeded-oat-bread',
+        amount: 35,
+        image: "https://i.postimg.cc/63dyHm59/super-seeded-oat-bread-2.jpg"
+    },
+    {
+        id: 2,
+        name: 'Crunchies',
+        amount: 12,
+        image: "https://i.postimg.cc/DfBYDYXy/crunchies-12.jpg"
+    },
+    {
+        id: 3,
+        name: 'German Chocolate Cake',
+        amount: 650,
+        image: "https://i.postimg.cc/Y9XfXZy5/german-chocolate-cake-177122-1.jpg"
+
+    },
+    {
+        id: 4,
+        name: 'Crazy Brownies',
+        amount: 9,
+        image: "https://i.postimg.cc/8c04C9RR/crazy-brownies-1.jpg"
+    },
+    {
+        id: 5,
+        name: 'Homemade-artisan-bread',
+        amount: 18,
+        image: "https://i.postimg.cc/RV1L3g7x/homemade-artisan-bread.jpg"
+    },
+    {
+        id: 6,
+        name: 'Santa-fe-cafe-tune-up-cafe breakfast',
+        amount: 50,
+        image: "https://i.postimg.cc/fWK6sR6s/best-breakfast-santa-fe-cafe-tune-up-cafe-fwx-298bbf750723481eabb95d3edbe7895d.jpg"
+    },
+    {
+        id: 7,
+        name: 'Chocolate Lamington',
+        amount: 12,
+        image: "https://i.postimg.cc/tTdMz6jg/lamington-recipe-ft.jpg"
+    },
+    {
+        id: 8,
+        name: 'Scones',
+        amount: 6,
+        image: "https://i.postimg.cc/tTDzTNbF/Me-Feb-2.jpg"
+    },
+    {
+        id: 9,
+        name: 'Breakfast with coffee',
+        amount: 40,
+        image: "https://i.postimg.cc/k4MxkRSG/the-best-coffee-and-breakfast.jpg"
+    }
+];
+localStorage.setItem('products', JSON.stringify(products))
+
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('searchInput');
     const sortSelect = document.getElementById('sortSelect');
     const productList = document.getElementById('productList');
-
-    //  product data sample  
-    let products =JSON.parse(localStorage.getItem('products'))?
-    JSON.parse(localStorage.getItem('products')) :
-    localStorage.setItem('products', JSON.stringify) [
-        // product data
-        {
-            id: 1,
-            name: 'Super-seeded-oat-bread',
-            amount: 35,
-            image:"https://i.postimg.cc/63dyHm59/super-seeded-oat-bread-2.jpg"
-            },
-            {
-            id: 2,
-            name: 'Crunchies',
-            amount: 12,
-            image:"https://i.postimg.cc/DfBYDYXy/crunchies-12.jpg"
-            },
-            {
-                id: 3,
-                name: 'German Chocolate Cake',
-                amount: 650,
-                image:"https://i.postimg.cc/Y9XfXZy5/german-chocolate-cake-177122-1.jpg"
-                
-                },
-                {
-                    id: 4,
-                    name: 'Crazy Brownies',
-                    amount: 9,
-                    image:"https://i.postimg.cc/8c04C9RR/crazy-brownies-1.jpg"
-                    },
-                    {
-                        id: 5,
-                        name: 'Homemade-artisan-bread',
-                        amount:18,
-                        image:"https://i.postimg.cc/RV1L3g7x/homemade-artisan-bread.jpg"
-                        },
-                        {
-                            id: 6,
-                            name: 'Santa-fe-cafe-tune-up-cafe breakfast',
-                            amount:50,
-                            image:"https://i.postimg.cc/fWK6sR6s/best-breakfast-santa-fe-cafe-tune-up-cafe-fwx-298bbf750723481eabb95d3edbe7895d.jpg"
-                            }
-    ];
 
     // Initial rendering of products
     renderProducts(products);
@@ -60,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function renderProducts(products) {
         productList.innerHTML = '';
         // ?
-        if(products.length) {
+        if (products.length) {
             products.forEach(product => {
                 const li = document.createElement('li');
                 li.classList.add('product-item');
@@ -72,13 +91,13 @@ document.addEventListener('DOMContentLoaded', function () {
                   <p class="card-text">R${product.amount} </p>
                 </div>
                 <div class="d-grid d-md-block ">
-                    <button class="btn btn-primary" type="button">Add to cart</button>
+                    <button class="btn btn-primary" type="button" onclick='addToCart(${JSON.stringify(product)})'>Add to cart</button>
                   </div>
             </div>
                 `;
                 productList.appendChild(li);
             });
-        }else {
+        } else {
             productList.innerHTML = 'Product was not found';
         }
     }
@@ -99,3 +118,10 @@ document.addEventListener('DOMContentLoaded', function () {
         renderProducts(sortedProducts);
     }
 });
+// Add to cart
+function addToCart(item) {
+    if(item) {
+        cart.push(item)
+        localStorage.setItem('checkout', JSON.stringify(cart))
+    }
+}
